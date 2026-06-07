88c1af6f6ceb7572ef56cc59766c9369973f2839
Dmitriy Zhuravlev <tankerlord@mail.ru>
2026-06-07 23:05:24 +0300

    shooter_4_1 Прицел, обычный и красный

---
+++ Assets/!Project/Scripts/Player/PlayerAim.cs
@@ -0,0 +1,38 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerAim : MonoBehaviour
+{
+    [SerializeField] Image _crossChair;
+    [SerializeField] private Transform _mainCamera;
+    [SerializeField] private float raycastDistance;
+    [SerializeField] private LayerMask _enemyLayerMask;
+
+    private float _lastCheckTime = 0f;
+    private float _checkInterval = 0.2f;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(_mainCamera.transform.position, _mainCamera.forward * raycastDistance);
+    }
+
+
+    void Update()
+    {
+        if (Time.time - _lastCheckTime > _checkInterval)
+        {
+            RaycastHit hit;
+            bool hasHit = Physics.Raycast(_mainCamera.transform.position, _mainCamera.forward, out hit, raycastDistance, _enemyLayerMask, QueryTriggerInteraction.Ignore);
+            if (hit.collider && hit.collider.gameObject.TryGetComponent<EnemyHealth>(out var _))
+            {
+                _crossChair.gameObject.SetActive(true);
+            }
+            else
+            {
+                _crossChair.gameObject.SetActive(false);
+            }
+            _lastCheckTime = Time.time;
+        }
+    }
+}
